ec0635b932d96d88a8e1d8d307626d0f2c05b66d
Roshantigga <99roshantigga@gmail.com>
2025-09-19 19:39:34 +0530

    Stastic Responce

---
+++ new pra/WebRooshan/WebRooshan/Controllers/HelloController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebRooshan.Controllers
+{
+    //[Route("hello")] //without this it will be conventional routing the url will go like : localhost:5001/{nameof class i.e. HelloController1/Index} like this other wise it will Attribute based here                    //url: localhost:5001/helloworld should write the url like this 
+    public class HelloController1 : Controller
+    {
+        //[HttpGet("")]
+        //GET: /<CONTROLLER>/
+        public IActionResult Index()
+        {
+            string html = "<h1> Hello world!</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
---
+++ new pra/WebRooshan/WebRooshan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebRooshan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
